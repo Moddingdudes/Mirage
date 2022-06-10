8c802c6a03c69fcfc0e353047f3cbed4477d961b
James Frowen <jamesfrowendev@gmail.com>
2022-06-10 19:20:34 +0100

    CI: moving generation to its own file

--- Assets/Mirage/Editor/NetworkMenu.cs
+++ Assets/Mirage/Editor/NetworkMenu.cs
@@ -2,7 +2,6 @@ using System;
 using Mirage.Logging;
 using Mirage.SocketLayer;
 using Mirage.Sockets.Udp;
-using Unity.CodeEditor;
 using UnityEditor;
 using UnityEngine;
 
@@ -10,51 +9,6 @@ namespace Mirage
 {
     public static class NetworkMenu
     {
-        public static void CreateSolution()
-        {
-            Debug.Log($"CreateSolution Start");
-            try
-            {
-                Debug.Log($"UnityEditor.SyncVS.SyncSolution");
-                var syncVS = typeof(CodeEditor).Assembly.GetType("UnityEditor.SyncVS");
-                var syncSln = syncVS.GetMethod("SyncSolution", System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.NonPublic);
-                syncSln.Invoke(null, null);
-            }
-            catch(Exception e)
-            {
-                Debug.LogException(e);
-            }
-
-            CodeEditor.Editor.CurrentCodeEditor.SyncAll();
-            CodeEditor.CurrentEditor.SyncAll();
-
-
-            try
-            {
-                Debug.Log($"UnityEditor.CodeEditorProjectSync");
-                AssetDatabase.Refresh();
-                CodeEditor.Editor.CurrentCodeEditor.SyncAll();
-                CodeEditor.Editor.CurrentCodeEditor.OpenProject("", -1, -1);
-            }
-            catch (Exception e)
-            {
-                Debug.LogException(e);
-            }
-
-
-            Debug.Log($"CodeEditor.Editor {CodeEditor.Editor}");
-            Debug.Log($"CodeEditor.Editor typeof {CodeEditor.Editor.GetType()}");
-            Debug.Log($"CodeEditor.CurrentEditor {CodeEditor.CurrentEditor}");
-            Debug.Log($"CodeEditor.Editor.CurrentCodeEditor {CodeEditor.Editor.CurrentCodeEditor}");
-            Debug.Log($"CodeEditor.Editor.CurrentCodeEditor.Installations.Length {CodeEditor.Editor.CurrentCodeEditor.Installations.Length}");
-            for (int i = 0; i < CodeEditor.Editor.CurrentCodeEditor.Installations.Length; i++)
-            {
-                Debug.Log($"CodeEditor.Editor.CurrentCodeEditor.Installations[{i}]: {CodeEditor.Editor.CurrentCodeEditor.Installations[i]}");
-            }
-
-            Debug.Log($"CreateSolution End");
-        }
-
         /// <summary>
         /// Creates a new game object with NetworkManager and other network components attached, Including UdpSocketFactory
         /// </summary>
